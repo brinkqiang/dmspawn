be4846ef99e6cd1ff74f98ca3b68511f533071fa
brinkqiang <brink.qiang@gmail.com>
2025-03-15 02:40:27 +0800

    add

--- build_mingw.bat
+++ build_mingw.bat
@@ -3,7 +3,7 @@
 rmdir /S /Q build 2> nul
 mkdir build
 pushd build
-cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=relwithdebinfo -DCMAKE_TOOLCHAIN_FILE=%VCPKG_ROOT%/scripts/buildsystems/vcpkg.cmake -DCMAKE_BUILD_TYPE=relwithdebinfo -DVCPKG_TARGET_TRIPLET=x64-windows ..
+cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=relwithdebinfo -DCMAKE_TOOLCHAIN_FILE=%VCPKG_ROOT%/scripts/buildsystems/vcpkg.cmake -DCMAKE_BUILD_TYPE=relwithdebinfo -DVCPKG_TARGET_TRIPLET=x64-mingw-static ..
 cmake --build . --config relwithdebinfo
 popd
 
